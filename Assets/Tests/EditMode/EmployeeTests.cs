affe6b79a99fbac13a9a8d3cca906765e62a2fdc
sakho13 <wecl.khotaro@gmail.com>
2026-08-31 22:27:22 +0900

    ✨ 社員ごとのタスク割り当てを実装

--- Assets/Tests/EditMode/EmployeeTests.cs
+++ Assets/Tests/EditMode/EmployeeTests.cs
@@ -23,4 +23,66 @@ public class EmployeeTests
     {
         Assert.Throws<ArgumentOutOfRangeException>(() => new Employee("ハム田", -5));
     }
+
+    [Test]
+    public void Constructor_SetsAvailableHoursToWeeklyAvailableHours()
+    {
+        var employee = new Employee("ハム田", 40);
+
+        Assert.AreEqual(40, employee.AvailableHours);
+    }
+
+    [Test]
+    public void TryConsumeHours_ValidAmount_ReducesAvailableHoursAndReturnsTrue()
+    {
+        var employee = new Employee("ハム田", 40);
+
+        var result = employee.TryConsumeHours(30);
+
+        Assert.IsTrue(result);
+        Assert.AreEqual(10, employee.AvailableHours);
+    }
+
+    [Test]
+    public void TryConsumeHours_ZeroOrNegative_ReturnsFalseAndDoesNotChangeAvailableHours()
+    {
+        var employee = new Employee("ハム田", 40);
+
+        Assert.IsFalse(employee.TryConsumeHours(0));
+        Assert.IsFalse(employee.TryConsumeHours(-10));
+        Assert.AreEqual(40, employee.AvailableHours);
+    }
+
+    [Test]
+    public void TryConsumeHours_ExceedsAvailableHours_ReturnsFalseAndDoesNotChangeAvailableHours()
+    {
+        var employee = new Employee("ハム田", 40);
+
+        var result = employee.TryConsumeHours(41);
+
+        Assert.IsFalse(result);
+        Assert.AreEqual(40, employee.AvailableHours);
+    }
+
+    [Test]
+    public void TryConsumeHours_MultipleCalls_AccumulateCorrectly()
+    {
+        var employee = new Employee("ハム田", 40);
+
+        Assert.IsTrue(employee.TryConsumeHours(30));
+        Assert.IsTrue(employee.TryConsumeHours(10));
+        Assert.AreEqual(0, employee.AvailableHours);
+        Assert.IsFalse(employee.TryConsumeHours(1));
+    }
+
+    [Test]
+    public void ResetWeeklyHours_RestoresAvailableHoursToWeeklyAvailableHours()
+    {
+        var employee = new Employee("ハム田", 40);
+        employee.TryConsumeHours(30);
+
+        employee.ResetWeeklyHours();
+
+        Assert.AreEqual(40, employee.AvailableHours);
+    }
 }
--- Assets/Tests/EditMode/GameSimulationTests.cs
+++ Assets/Tests/EditMode/GameSimulationTests.cs
@@ -40,123 +40,200 @@ public class GameSimulationTests
     }
 
     [Test]
-    public void TryAllocateHours_MultipleTasks_Succeeds()
+    public void TryAllocateHours_SingleEmployeeMultipleTasks_Succeeds()
     {
         var simulation = new GameSimulation();
-        simulation.AddEmployee(new Employee("ハム田", 40));
+        var hamda = new Employee("ハム田", 40);
+        simulation.AddEmployee(hamda);
         var login = new DevelopmentTask("ログイン機能", 30);
         var csv = new DevelopmentTask("CSV出力", 40);
         simulation.AddTask(login);
         simulation.AddTask(csv);
 
-        var result1 = simulation.TryAllocateHours(login, 20);
-        var result2 = simulation.TryAllocateHours(csv, 20);
+        var result1 = simulation.TryAllocateHours(hamda, login, 20);
+        var result2 = simulation.TryAllocateHours(hamda, csv, 20);
 
         Assert.IsTrue(result1);
         Assert.IsTrue(result2);
         Assert.AreEqual(40, simulation.AllocatedHours);
-        Assert.AreEqual(0, simulation.AvailableHours);
+        Assert.AreEqual(0, hamda.AvailableHours);
     }
 
     [Test]
-    public void TryAllocateHours_ExceedsAvailableHours_ReturnsFalse()
+    public void TryAllocateHours_MultipleEmployeesSameTask_Succeeds()
     {
         var simulation = new GameSimulation();
-        simulation.AddEmployee(new Employee("ハム田", 40));
+        var hamda = new Employee("ハム田", 40);
+        var hamkawa = new Employee("ハム川", 40);
+        simulation.AddEmployee(hamda);
+        simulation.AddEmployee(hamkawa);
+        var login = new DevelopmentTask("ログイン機能", 60);
+        simulation.AddTask(login);
+
+        var result1 = simulation.TryAllocateHours(hamda, login, 30);
+        var result2 = simulation.TryAllocateHours(hamkawa, login, 20);
+
+        Assert.IsTrue(result1);
+        Assert.IsTrue(result2);
+        Assert.AreEqual(50, simulation.AllocatedHours);
+    }
+
+    [Test]
+    public void TryAllocateHours_ExceedsEmployeeWeeklyHours_ReturnsFalse()
+    {
+        var simulation = new GameSimulation();
+        var hamda = new Employee("ハム田", 40);
+        simulation.AddEmployee(hamda);
         var task = new DevelopmentTask("CSV出力", 100);
         simulation.AddTask(task);
 
-        var result = simulation.TryAllocateHours(task, 41);
+        var result = simulation.TryAllocateHours(hamda, task, 41);
 
         Assert.IsFalse(result);
         Assert.AreEqual(0, simulation.AllocatedHours);
-        Assert.AreEqual(40, simulation.AvailableHours);
+        Assert.AreEqual(40, hamda.AvailableHours);
     }
 
     [Test]
     public void TryAllocateHours_ZeroOrNegativeHours_ReturnsFalse()
     {
         var simulation = new GameSimulation();
-        simulation.AddEmployee(new Employee("ハム田", 40));
+        var hamda = new Employee("ハム田", 40);
+        simulation.AddEmployee(hamda);
         var task = new DevelopmentTask("CSV出力", 100);
         simulation.AddTask(task);
 
-        Assert.IsFalse(simulation.TryAllocateHours(task, 0));
-        Assert.IsFalse(simulation.TryAllocateHours(task, -10));
+        Assert.IsFalse(simulation.TryAllocateHours(hamda, task, 0));
+        Assert.IsFalse(simulation.TryAllocateHours(hamda, task, -10));
     }
 
     [Test]
-    public void TryAllocateHours_NoEmployeesRegistered_ReturnsFalse()
+    public void TryAllocateHours_UnregisteredEmployee_ReturnsFalse()
     {
         var simulation = new GameSimulation();
-        var task = new DevelopmentTask("CSV出力", 100);
-        simulation.AddTask(task);
+        var registeredTask = new DevelopmentTask("CSV出力", 100);
+        simulation.AddTask(registeredTask);
+        var unregisteredEmployee = new Employee("ハム鈴", 40);
 
-        var result = simulation.TryAllocateHours(task, 10);
+        var result = simulation.TryAllocateHours(unregisteredEmployee, registeredTask, 10);
 
         Assert.IsFalse(result);
+        Assert.AreEqual(40, unregisteredEmployee.AvailableHours);
+        Assert.AreEqual(0, simulation.AllocatedHours);
     }
 
     [Test]
-    public void AdvanceWeek_ReflectsAllocatedHoursIntoTaskProgress()
+    public void TryAllocateHours_UnregisteredTask_ReturnsFalse()
     {
         var simulation = new GameSimulation();
-        simulation.AddEmployee(new Employee("ハム田", 40));
-        var task = new DevelopmentTask("ログイン機能", 30);
+        var hamda = new Employee("ハム田", 40);
+        simulation.AddEmployee(hamda);
+        var unregisteredTask = new DevelopmentTask("未登録タスク", 100);
+
+        var result = simulation.TryAllocateHours(hamda, unregisteredTask, 10);
+
+        Assert.IsFalse(result);
+        Assert.AreEqual(40, hamda.AvailableHours);
+        Assert.AreEqual(0, simulation.AllocatedHours);
+    }
+
+    [Test]
+    public void TryAllocateHours_CompletedTask_ReturnsFalse()
+    {
+        var simulation = new GameSimulation();
+        var hamda = new Employee("ハム田", 40);
+        simulation.AddEmployee(hamda);
+        var task = new DevelopmentTask("小さいタスク", 10);
         simulation.AddTask(task);
-        simulation.TryAllocateHours(task, 20);
+        simulation.TryAllocateHours(hamda, task, 10);
+        simulation.AdvanceWeek();
+
+        Assert.IsTrue(task.IsCompleted);
+
+        var result = simulation.TryAllocateHours(hamda, task, 5);
+
+        Assert.IsFalse(result);
+        Assert.AreEqual(40, hamda.AvailableHours);
+    }
+
+    [Test]
+    public void AdvanceWeek_SumsMultipleEmployeeAllocationsIntoTaskProgress()
+    {
+        var simulation = new GameSimulation();
+        var hamda = new Employee("ハム田", 40);
+        var hamkawa = new Employee("ハム川", 40);
+        simulation.AddEmployee(hamda);
+        simulation.AddEmployee(hamkawa);
+        var login = new DevelopmentTask("ログイン機能", 60);
+        simulation.AddTask(login);
+        simulation.TryAllocateHours(hamda, login, 30);
+        simulation.TryAllocateHours(hamkawa, login, 20);
 
         simulation.AdvanceWeek();
 
-        Assert.AreEqual(20, task.CompletedHours);
+        Assert.AreEqual(50, login.CompletedHours);
     }
 
     [Test]
-    public void AdvanceWeek_ResetsAvailableHoursToEmployeeTotal()
+    public void AdvanceWeek_ResetsEachEmployeeAndTeamAvailableHours()
     {
         var simulation = new GameSimulation();
-        simulation.AddEmployee(new Employee("ハム田", 40));
-        var task = new DevelopmentTask("ログイン機能", 30);
+        var hamda = new Employee("ハム田", 40);
+        var hamkawa = new Employee("ハム川", 40);
+        simulation.AddEmployee(hamda);
+        simulation.AddEmployee(hamkawa);
+        var task = new DevelopmentTask("ログイン機能", 60);
         simulation.AddTask(task);
-        simulation.TryAllocateHours(task, 20);
+        simulation.TryAllocateHours(hamda, task, 30);
+        simulation.TryAllocateHours(hamkawa, task, 20);
 
-        Assert.AreEqual(20, simulation.AvailableHours);
+        Assert.AreEqual(10, hamda.AvailableHours);
+        Assert.AreEqual(20, hamkawa.AvailableHours);
 
         simulation.AdvanceWeek();
 
-        Assert.AreEqual(40, simulation.AvailableHours);
+        Assert.AreEqual(40, hamda.AvailableHours);
+        Assert.AreEqual(40, hamkawa.AvailableHours);
+        Assert.AreEqual(80, simulation.AvailableHours);
     }
 
     [Test]
-    public void AdvanceWeek_ClearsAllocationState()
+    public void AdvanceWeek_ClearsPreviousWeekAllocationState()
     {
         var simulation = new GameSimulation();
-        simulation.AddEmployee(new Employee("ハム田", 40));
-        var task = new DevelopmentTask("ログイン機能", 30);
+        var hamda = new Employee("ハム田", 40);
+        simulation.AddEmployee(hamda);
+        var task = new DevelopmentTask("ログイン機能", 60);
         simulation.AddTask(task);
-        simulation.TryAllocateHours(task, 20);
+        simulation.TryAllocateHours(hamda, task, 20);
 
         simulation.AdvanceWeek();
 
         Assert.AreEqual(0, simulation.AllocatedHours);
+
+        var result = simulation.TryAllocateHours(hamda, task, 40);
+        Assert.IsTrue(result);
     }
 
     [Test]
     public void AdvanceWeek_MatchesIssueExampleScenario()
     {
         var simulation = new GameSimulation();
-        simulation.AddEmployee(new Employee("ハム田", 40));
-        simulation.AddEmployee(new Employee("ハム川", 40));
-        var login = new DevelopmentTask("ログイン機能", 50);
-        var csv = new DevelopmentTask("CSV出力", 30);
+        var hamda = new Employee("ハム田", 40);
+        var hamkawa = new Employee("ハム川", 40);
+        simulation.AddEmployee(hamda);
+        simulation.AddEmployee(hamkawa);
+        var login = new DevelopmentTask("ログイン機能", 60);
+        var csv = new DevelopmentTask("CSV出力", 40);
         simulation.AddTask(login);
         simulation.AddTask(csv);
 
-        Assert.AreEqual(80, simulation.AvailableHours);
+        simulation.TryAllocateHours(hamda, login, 30);
+        simulation.TryAllocateHours(hamda, csv, 10);
+        simulation.TryAllocateHours(hamkawa, login, 20);
+        simulation.TryAllocateHours(hamkawa, csv, 20);
 
-        simulation.TryAllocateHours(login, 50);
-        simulation.TryAllocateHours(csv, 30);
-
+        Assert.AreEqual(80, simulation.AllocatedHours);
         Assert.AreEqual(0, simulation.AvailableHours);
 
         simulation.AdvanceWeek();
